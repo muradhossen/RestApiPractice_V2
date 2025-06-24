fff0540723642ed3082155b93618097427ef66a0
muradhossen <hossenmurad46@gmail.com>
2025-06-24 23:02:39 +0600

    added auth and refresh token in sdk

---
+++ Movies.Api.Sdk.Consumer/AuthHeaderHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Api.Sdk.Consumer
+{
+    internal class AuthHeaderHandler : DelegatingHandler
+    {
+        private readonly AuthTokenProvider _tokenProvider;
+
+        public AuthHeaderHandler(AuthTokenProvider tokenProvider)
+        {
+            _tokenProvider = tokenProvider;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _tokenProvider.GetTokenAsync();
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
--- Movies.Api.Sdk.Consumer/Program.cs
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -2,17 +2,25 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Movies.Api.Sdk;
+using Movies.Api.Sdk.Consumer;
 using Movies.Contracts.Requests;
 using Refit;
 using System.Text.Json;
 
 //var moviesApi = RestService.For<IMoviesApi>("https://localhost:7196");
 
-//var movie =await moviesApi.GetMovieAsync("a69786f2-1f2d-40bc-8d84-d5b77bdfd35b");
+
 
 var services = new ServiceCollection();
 
-services.AddRefitClient<IMoviesApi>()
+
+
+services
+    .AddSingleton<AuthTokenProvider>()
+    .AddTransient<AuthHeaderHandler>() // Register handler
+    .AddRefitClient<IMoviesApi>()
+        .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://localhost:5001"))
+        .AddHttpMessageHandler<AuthHeaderHandler>()
     .ConfigureHttpClient(x =>
     {
         x.BaseAddress = new Uri("https://localhost:5001");
@@ -22,11 +30,13 @@ var provider = services.BuildServiceProvider();
 
 var moviesApi = provider.GetRequiredService<IMoviesApi>();
 
-var movies = await moviesApi.GetMoviesAsync(new GetAllMoviesRequest { Page = 1, PageSize = 3 });
+var movie = await moviesApi.GetMovieAsync("a69786f2-1f2d-40bc-8d84-d5b77bdfd35b");
 
-foreach (var item in movies.Items)
-{
-    Console.WriteLine(JsonSerializer.Serialize(item));
-}
+//var movies = await moviesApi.GetMoviesAsync(new GetAllMoviesRequest { Page = 1, PageSize = 3 });
+
+//foreach (var item in movies.Items)
+//{
+//    Console.WriteLine(JsonSerializer.Serialize(item));
+//}
 
 Console.ReadLine();
