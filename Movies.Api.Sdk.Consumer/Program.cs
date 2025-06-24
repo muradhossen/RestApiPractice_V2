e2304d71e404316bf8fb0527e352cf4498466ea8
murad <murad@shadhinmusic.com>
2025-06-24 22:03:56 +0600

    sdk : added refit

---
+++ Movies.Api.Sdk.Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using Movies.Api.Sdk;
+using Movies.Contracts.Requests;
+using Refit;
+using System.Text.Json;
+
+//var moviesApi = RestService.For<IMoviesApi>("https://localhost:7196");
+
+//var movie =await moviesApi.GetMovieAsync("a69786f2-1f2d-40bc-8d84-d5b77bdfd35b");
+
+var services = new ServiceCollection();
+
+services.AddRefitClient<IMoviesApi>()
+    .ConfigureHttpClient(x =>
+    {
+        x.BaseAddress = new Uri("https://localhost:5001");
+    });
+
+var provider = services.BuildServiceProvider();
+
+var moviesApi = provider.GetRequiredService<IMoviesApi>();
+
+var movies = await moviesApi.GetMoviesAsync(new GetAllMoviesRequest { Page = 1, PageSize = 3 });
+
+foreach (var item in movies.Items)
+{
+    Console.WriteLine(JsonSerializer.Serialize(item));
+}
+
+Console.ReadLine();
---
+++ Movies.Api.Sdk/IMoviesApi.cs
@@ -0,0 +1,13 @@
+﻿using Movies.Contracts.Requests;
+using Movies.Contracts.Responses;
+using Refit;
+
+namespace Movies.Api.Sdk;
+
+public interface IMoviesApi
+{
+    [Get(ApiEndpoints.Default.Movies.Get)] Task<MovieResponse> GetMovieAsync(string idOrSlug);
+
+    [Get(ApiEndpoints.Default.Movies.GetAll)] Task<MoviesResponse> GetMoviesAsync(GetAllMoviesRequest request);
+
+}
